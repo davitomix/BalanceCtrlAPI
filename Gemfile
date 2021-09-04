3b5f8f0a963a7b64f2bb4d1985231780feb76bee
David Martinez <davidelimg1@gmail.com>
2021-09-04 10:26:39 -0500

    [feat]: fixed and added env vars

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'rswag'
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'database_cleaner-active_record'
+  gem 'dotenv-rails'
   gem 'guard'
   gem 'guard-rspec', require: false
   gem 'pry-byebug'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.allowed_cors_origins = ENV['FRONTEND_HOST']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
